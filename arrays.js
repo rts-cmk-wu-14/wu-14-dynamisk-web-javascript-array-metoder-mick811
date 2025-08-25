54cfbe1f50be40e4c5f9420a1004e8c45d5190f7
mick811 <mikkelelvers@outlook.com>
2025-08-25 09:11:28 +0200

    resolved q3a & q3b

--- arrays.js
+++ arrays.js
@@ -21,12 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
     // Check om der i dogsArray er en hund som hedder "Trixie".
     // Udskriv "true" eller "false" i konsollen afhængig af svaret.
     // console.log("Opgave 3a", ...)
-    console.log("Opgave 3", dogsArray.includes("Trixie"))
+    console.log("Opgave 3a", dogsArray.includes("Trixie"))
 
     // Opgave 3b
     // Check om der i dogsArray er en hund som hedder "Baxter".
     // Udskriv "true" eller "false" i konsollen afhængig af svaret.
     // console.log("Opgave 3b", ...)
+    console.log("Opgave 3b", dogsArray.includes("Baxter"))
 
     //     Opgave 4a
     // Check om der i catsArray er en kat som hedder "Bagheera".
